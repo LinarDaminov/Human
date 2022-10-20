b07e14517963b382a9956c66186e0fa242d09fa2
LinarDaminov <linar.daminov@yandex.ru>
2022-10-20 05:59:56 +0500

    OOP2,HomeWork,time-out

---
+++ src/Flowers.java
@@ -0,0 +1,8 @@
+public class Flowers {
+    private String color;
+    private String country;
+    private int cost;
+    private int lifeSpan;
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ public class Main {
         Human Anna = new Human("Аня", 29, "Москва", "Методист образовательных программ");
         Human Katya = new Human("Катя", 28, "Калининград", "Продакт- менеджер");
         Human Artem = new Human("Артем", 27, "Москва", "Директор по развитию бизнеса");
+        Human Vladimir = new Human("Владимир", 21, "Казань", "Безработный");
+
+
         System.out.println(Maksim.toString());
         System.out.println(Anna.toString());
         System.out.println(Katya.toString());
         System.out.println(Artem.toString());
+        System.out.println(Vladimir.toString());
         System.out.println();
         Auto lada = new Auto("lada", "Granta", 1.7, "Желтый", 2015, "Россия");
         Auto audi = new Auto("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
